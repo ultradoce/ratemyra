d3d9211c2686c0699f7c6ffe00e984216fedc048
ultradoce <115197779+ultradoce@users.noreply.github.com>
2026-01-22 18:49:37 -0500

    Fix: Add missing Link import in Home.jsx

--- Documents/ratemyra/client/src/pages/Home.jsx
+++ Documents/ratemyra/client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
